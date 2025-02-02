703faa76c284e781b1cb4f5193751d0380e0a418
lamine <lamined34@gmail.com>
2025-02-02 12:59:41 +0000

    setup

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className='flex justify-center items-center h-screen p-10 bg-gray-100'>
+        <p>Hello world</p>
+      </div>
+    </>
+  )
+}
+
+export default App
