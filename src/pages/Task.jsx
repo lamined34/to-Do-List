6b011849c4f3fec5c76bf2b0e6844e87836478a8
lamine <lamined34@gmail.com>
2025-02-03 02:48:43 +0000

    refining the add a task page and adding more animation

--- src/pages/Task.jsx
+++ src/pages/Task.jsx
@@ -1,6 +1,6 @@
 import { tasksExamples } from "../data/taskData";
 import TaskList from "../components/TaskList";
-import { Plus, ClipboardList, CheckCircle,  Clock } from "lucide-react";
+import { Plus, ClipboardList, CheckCircle, Clock } from "lucide-react";
 import React, { useEffect } from "react";
 import TaskAdd from "./TaskAdd";
 import CountCard from "../components/CountCard";
@@ -46,7 +46,7 @@ const Task = () => {
             id: getBiggestId() + 1,
             title: task.title,
             description: task.description,
-            status: 'termine',
+            status: 'en cours',
             start: new Date().toLocaleDateString('fr-Fr'),
             end: null,
         }
@@ -84,62 +84,66 @@ const Task = () => {
 
 
     return (
-        <div className="absolute flex flex-col w-full max-w-lg h-screen bg-white overflow-scroll gap-10 py-10 px-6 md:px-10">
-            <h1 className={`text-center text-5xl md:text-7xl text-gray-800 underline underline-offset-4 opacity-0
-                transition duration-500 ${showTasks && 'opacity-100'}`}>
-                To Do List
-            </h1>
-
-            <div className={`grid grid-cols-3 gap-2.5 md:gap-5 opacity-0 transition-all duration-500 ease-in-out ${showTasks && 'opacity-100'}`}>
-                <CountCard title={'Total'} Icon={ClipboardList} count={getTheCount(tasks, 'all')}/>
-                <CountCard title={'En cours'} Icon={Clock} count={getTheCount(tasks, 'en cours')} />
-                <CountCard title={'Terminées'} Icon={CheckCircle} count={getTheCount(tasks, 'termine')} />
-            </div>
-
+        <>
             {
                 showAddTask ? (
                     <TaskAdd onSubmitTask={handleNewTask} />
                 ) : (
-                    <div>
-                        <div className={`py-2 mb-2 border-b border-gray-200 max-w-fit -100  opacity-0 transition duration-500 ${showTasks && 'opacity-100'}`}>
-                            <label htmlFor="filterbyStatus" className="text-2xl font-semibold text-gray-700 px-2">
-                                Filter: 
-                            </label>
+                    <div className="absolute flex flex-col w-full max-w-lg h-screen bg-white overflow-scroll gap-10 py-10 px-6 md:px-10">
+                        <h1 className={`text-center text-5xl md:text-7xl text-gray-800 underline underline-offset-4 opacity-0
+                transition duration-300 ${showTasks && 'opacity-100'}`}>
+                            To Do List
+                        </h1>
 
-                            <select name="filter" id="filterbyStatus" onChange={handleFilter}
-                                className="text-center text-lg capitalize italic outline-0">
-                                <option value="all">Toutes</option>
-                                <option value="en cours">En cours</option>
-                                <option value="termine">Terminées</option>
-                            </select>
-
+                        <div className={`grid grid-cols-3 gap-2.5 md:gap-5 opacity-0 transition-all duration-300 ease-in ${showTasks && 'opacity-100'}`}>
+                            <CountCard title={'Total'} Icon={ClipboardList} count={getTheCount(tasks, 'all')} />
+                            <CountCard title={'En cours'} Icon={Clock} count={getTheCount(tasks, 'en cours')} />
+                            <CountCard title={'Terminées'} Icon={CheckCircle} count={getTheCount(tasks, 'termine')} />
                         </div>
 
-                        <div className={`relative transition scale-y-0 origin-top duration-500 ease-in-out ${showTasks && 'scale-y-100'}`} >
-                            {
-                                !tasks ? (
-                                    <p className="text-center text-sm text-gray-600">Aucune tâche</p>
-                                ) : (
-                                    <TaskList list={filteredTasks} handleRemoveItem={handleRemoveItem} />
-                                )
-                            }
-                        </div>
+                        <div>
+                            <div className={`py-2 mb-2 border-b border-gray-200 max-w-fit -100  opacity-0 transition duration-500 ${showTasks && 'opacity-100'}`}>
+                                <label htmlFor="filterbyStatus" className="text-2xl font-semibold text-gray-700 px-2">
+                                    Filter:
+                                </label>
 
-                        <button
-                            onClick={() => {
-                                setShowAddTask(true)
-                                setShowTask(false)
-                            }}
-                            className="fixed z-20 bottom-16 shadow-xl h-16 w-16 bg-gray-50 border-2 border-gray-100
+                                <select name="filter" id="filterbyStatus" onChange={handleFilter}
+                                    className="text-center text-lg capitalize italic outline-0">
+                                    <option value="all">Toutes</option>
+                                    <option value="en cours">En cours</option>
+                                    <option value="termine">Terminées</option>
+                                </select>
+
+                            </div>
+
+                            <div className={`relative transition scale-y-0 origin-top duration-500 ease-in-out ${showTasks && 'scale-y-100'}`} >
+                                {
+                                    !tasks ? (
+                                        <p className="text-center text-sm text-gray-600">Aucune tâche</p>
+                                    ) : (
+                                        <TaskList list={filteredTasks} handleRemoveItem={handleRemoveItem} />
+                                    )
+                                }
+                            </div>
+
+                            <button
+                                onClick={() => {
+                                    setShowAddTask(true)
+                                    setShowTask(false)
+                                }}
+                                className="fixed z-20 bottom-16 shadow-xl h-16 w-16 bg-gray-50 border-2 border-gray-100
                                         flex justify-center items-center rounded-full transition-all duration-300 ease-in-out hover:scale-125 hover:shadow-2xl
                                         hover:-translate-y-1 opacity-85">
-                            <Plus className="h-full w-full stroke-1" />
-                        </button>
+                                <Plus className="h-full w-full stroke-1" />
+                            </button>
+                        </div>
+
+
+
                     </div>
                 )
             }
-
-        </div>
+        </>
     );
 
 }
--- src/pages/TaskAdd.jsx
+++ src/pages/TaskAdd.jsx
@@ -6,24 +6,32 @@ const TaskAdd = ({onSubmitTask}) => {
 
     return (
         <>
-            <div className="bg-white w-full max-w-lg p-5 space-y-7">
-                <h1 className="text-4xl text-gray-700">Nouvelle Tâche</h1>
+            <div className="bg-white w-full max-w-lg p-5 space-y-7 pt-5">
+                <h1 className="text-4xl text-gray-800 mb-28 font-bold">Nouvelle Tâche</h1>
 
                 <form onSubmit={handleSubmit(onSubmitTask)}>
                     <div className="flex flex-col gap-5">
                         <div className="flex flex-col gap-4">
-                            <label htmlFor="title">Nom</label>
-                            <input type="text" id="title" {...register('title')} className="border-b mx-5" />
+                            <label htmlFor="title" className="text-xl text-gray-800 italic font-semibold">Nom</label>
+                            <input type="text" id="title" {...register('title')} className="border-b-2 outline-0 mx-6 px-0.5 border-gray-500" />
                         </div>
 
                         <div className="flex flex-col gap-4">
-                            <label htmlFor="description">Description</label>
-                            <textarea id="description" {...register('description')} className="border mx-5" />
+                            <label htmlFor="description" className="text-xl text-gray-800 italic font-semibold">Description</label>
+                            <textarea id="description" {...register('description')} className="border-2 outline-0 mx-6 px-0.5 rounded-md h-36 border-gray-500" />
                         </div>
 
-                        <div className="flex gap-2">
-                            <button type="button">Annuler</button>
-                            <button type="submit">Ajouter</button>
+                        <div className="flex justify-end mr-6 gap-6">
+                            <button type="button" className="outline-1 px-5 py-2.5 rounded-md outline-red-500 text-sm font-semibold 
+                            transition duration-300 ease-in-out hover:scale-x-110 hover:bg-red-500 hover:text-white
+                            hover:scale-y-105 active:scale-90 active:bg-red-500 active:text-white">
+                                Annuler
+                            </button>
+                            <button type="submit" className="outline-1 px-5 py-2.5 rounded-md outline-blue-500 text-sm font-semibold 
+                            transition duration-300 ease-in-out hover:bg-blue-500 hover:text-white hover:scale-x-110 
+                            hover:scale-y-105 active:scale-90 active:bg-blue-500 active:text-white">
+                                Ajouter
+                            </button>
                         </div>
                     </div>
                 </form>
